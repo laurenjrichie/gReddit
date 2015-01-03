6ea4d4ec6f77dba577327c0988563c8f54079826
laurenjrichie <laurenjrichie@gmail.com>
2015-01-03 18:08:32 -0500

    can edit a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,16 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    if @post.save
+      redirect_to post_path(@post), notice: "Post was successfully updated"
+    else
+      render :edit
+    end
+  end
+
   private
   def post_params
     params.require(:post).permit(
