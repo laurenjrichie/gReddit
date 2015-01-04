15f18701684e5c35dabeb56fbc4c8984c96ec875
laurenjrichie <laurenjrichie@gmail.com>
2015-01-03 19:43:34 -0500

    can delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,16 +4,25 @@ class CommentsController < ApplicationController
   end
 
   def new
+    # authenticate_user
     @comment = @post.comments.new
   end
 
   def create
+    # authenticate_user
     @comment = @post.comments.new(comment_params)
+    # @comment.user_id = current.user_id
     if @comment.save
       redirect_to @post, notice: "Your comment has been posted."
     else
       render :new
     end
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @post, notice: "Comment was successfully deleted."
   end
 
   private
